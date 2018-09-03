e96dcbc62369ab5095617462a2397dd53c135d28
Miguel Aniceto <lima.aniceto@gmail.com>
2018-09-03 16:08:20 +0100

    Update to CSS

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'font-awesome/css/font-awesome.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
 
 ReactDOM.render(
     <App/>

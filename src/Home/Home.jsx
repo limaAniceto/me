2066ba341eea94aedcc2ef982617e931985d036a
Miguel Aniceto <miguel.aniceto@mixcloud.com>
2018-11-04 12:45:04 +0000

    Updating info

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,80 +1,86 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 // This is a minor issue, take care of dynamically loading the images later.
-import career from './career.png';
-import tech from './tech.png';
-import work from './work.png';
+import career from "./career.png";
+import tech from "./tech.png";
+import work from "./work.png";
 
 const cards = [
-    {
-        label: 'Career',
-        key: 'career',
-        image: career,
-        text: "Since I started studying I've been learning something new about technology every day.",
-        list: ['Unix environments management, as a Sys-Admin assistant for the University',
-            'Web & Big Data Application Development for specific purposes in the Financial Services and Insurance industry, as Consultant for a Big4',
-            'Web applications for serving Big Data, widely used internaly by BNP Paribas'
-        ],
-    },
-    {
-        label: 'Technologies',
-        key: 'tech',
-        image: tech,
-        text: "Software Engineers have a large array of tools available, see below what I've been using:",
-        list: [
-            "Unix Operating System - It can be Ubuntu, CentOs or any main distro, this is the OS where I do most of my work. ",
-            "Web Frameworks - Angular and React (Only work with React at the moment)",
-            "Databases - Mostly NoSQL databases (DataStax, Redis) and SQL Server"
-        ]
-    },
-    {
-        label: 'Current Work',
-        key: 'currWork',
-        image: work,
-        text: "Currently I work for Mixcloud as a Javascript Engineer.",
-        list: ['Technologies - React, GraphQL, Apollo, Node.JS, D3.JS, React Native',
-            'Agile methodology with dailies featuring project sponsors, designer, data scientists and analysts',
-            'MicroServices architurecture, more specifically backends for frontends',
-            'Big data apps since each application serves large amounts of data']
-    }];
+  {
+    label: "Career",
+    key: "career",
+    image: career,
+    text:
+      "I've worked in the Financial and Insurance industry, currently I'm focused on pushing the music streaming industry at Mixcloud.",
+    list: [
+      "Unix environments management, as a Sys-Admin assistant for the University",
+      "Web & Big Data Application Development for specific purposes in the Financial Services and Insurance industry, as Consultant for a Big4",
+      "Web applications for serving Big Data, widely used internaly by BNP Paribas",
+      "Developing software for Mixcloud - The company that is changing the paradigm of Radio and Music shows."
+    ]
+  },
+  {
+    label: "Technologies",
+    key: "tech",
+    image: tech,
+    text:
+      "Having worked with Technology for more than 10 years, I've worked with quite some different environments and industries. See below what I've been using:",
+    list: [
+      "Unix OS",
+      "Web Frameworks - Angular and React (Only work with React at the moment)",
+      "Databases - Mostly NoSQL databases (DataStax, Redis) and SQL Server",
+      "Visualization Specific libs for the Web (2D and 3D)",
+      "ETL and setting up large scale environments such as Hadoop"
+    ]
+  },
+  {
+    label: "Current Work",
+    key: "currWork",
+    image: work,
+    text:
+      "Currently I work for Mixcloud as a Javascript Engineer. In short I work as a Full Stack Software Engineer, using the following technologies - React, GraphQL, Relay, React Native. These are some of the challenges I face:",
+    list: [
+      "Moving Relay Classic to Relay Modern, upgrading out-of-date libs and code",
+      "Demonstrating the best practices and implementing them.",
+      "Achieving a more collaborative codebase for both designers and engineers"
+    ]
+  }
+];
 
 const Home = () => (
-    <Container>
-        {
-            cards.map(card => {
-                return (
-                    <Card key={card.key}>
-                        <CardContent>
-                            <Avatar>
-                                <StyledImg src={card.image}></StyledImg>
-                            </Avatar>
-                            <h2>{card.label}</h2>
-                            <CardText>{card.text}</CardText>
-                            {card.list && <ul>
-                                {card.list.map((el, i) => {
-                                    return (
-                                        <li key={card.key + i}>{el}</li>
-                                    );
-                                })}
-                            </ul>
-                            }
-                        </CardContent>
-                    </Card>
-                );
-            })
-        }
-    </Container>
+  <Container>
+    {cards.map(card => {
+      return (
+        <Card key={card.key}>
+          <CardContent>
+            <Avatar>
+              <StyledImg src={card.image} />
+            </Avatar>
+            <h2>{card.label}</h2>
+            <CardText>{card.text}</CardText>
+            {card.list && (
+              <ul>
+                {card.list.map((el, i) => {
+                  return <li key={card.key + i}>{el}</li>;
+                })}
+              </ul>
+            )}
+          </CardContent>
+        </Card>
+      );
+    })}
+  </Container>
 );
 
 /* ----------------- Styles ----------------- */
 const Container = styled.div`
-    flex: 1;
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-    flex-wrap: wrap;
+  flex: 1;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
 `;
 const Card = styled.div`
     background-color: #fafafa;
@@ -84,8 +90,8 @@ const Card = styled.div`
     &:hover {
         box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
     }
-    min-height: 550px
-    max-height: 550px;
+    min-height: 600px
+    max-height: 600px;
     min-width: 300px;
     max-width: 300px;
     margin: 30px;
@@ -96,27 +102,27 @@ const Card = styled.div`
 
 `;
 const CardContent = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    margin: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 20px;
 `;
 const Avatar = styled.div`
-    background-color: white;
-    border-radius: 50%;
-    border: 3px solid grey;
-    width: 140px;
-    height: 140px;      
-    justify-content: center;
-    align-items: center;
-    display: flex;
+  background-color: white;
+  border-radius: 50%;
+  border: 3px solid grey;
+  width: 140px;
+  height: 140px;
+  justify-content: center;
+  align-items: center;
+  display: flex;
 `;
 const StyledImg = styled.img`
-    max-width: 60%;
+  max-width: 60%;
 `;
 
 const CardText = styled.p`
-    font-size: 1.2em;
+  font-size: 1.2em;
 `;
 export default Home;

4d8d8b38cb78c7992d3ee00528b3c0d14a51b1ae
Miguel Aniceto <miguel.aniceto@mixcloud.com>
2018-12-02 10:17:02 +0000

    Working three

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,11 +1,18 @@
 import React from "react";
-import styled from "styled-components";
 
 // This is a minor issue, take care of dynamically loading the images later.
 import career from "./career.png";
 import tech from "./tech.png";
 import work from "./work.png";
 
+import {
+  Card,
+  CardContent,
+  CardText,
+  Avatar,
+  StyledImg,
+  Container
+} from "../styles/styles";
 const cards = [
   {
     label: "Career",
@@ -73,56 +80,4 @@ const Home = () => (
   </Container>
 );
 
-/* ----------------- Styles ----------------- */
-const Container = styled.div`
-  flex: 1;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  flex-wrap: wrap;
-`;
-const Card = styled.div`
-    background-color: #fafafa;
-    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
-    transition: 0.3s;
-    border-radius: 5px;
-    &:hover {
-        box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
-    }
-    min-height: 600px
-    max-height: 600px;
-    min-width: 300px;
-    max-width: 300px;
-    margin: 30px;
-    &:hover {
-        box-shadow: 0 15px 15px rgba(0, 0, 0, .16);
-        transform: translate(0, -5px);
-    }
-
-`;
-const CardContent = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  margin: 20px;
-`;
-const Avatar = styled.div`
-  background-color: white;
-  border-radius: 50%;
-  border: 3px solid grey;
-  width: 140px;
-  height: 140px;
-  justify-content: center;
-  align-items: center;
-  display: flex;
-`;
-const StyledImg = styled.img`
-  max-width: 60%;
-`;
-
-const CardText = styled.p`
-  font-size: 1.2em;
-`;
 export default Home;
---
+++ src/styles/styles.js
@@ -0,0 +1,55 @@
+import styled from "styled-components";
+
+/* ----------------- Styles ----------------- */
+const Container = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+`;
+const Card = styled.div`
+    background-color: #fafafa;
+    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+    transition: 0.3s;
+    border-radius: 5px;
+    &:hover {
+        box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+    }
+    min-height: 600px
+    max-height: 600px;
+    min-width: 300px;
+    max-width: 300px;
+    margin: 30px;
+    &:hover {
+        box-shadow: 0 15px 15px rgba(0, 0, 0, .16);
+        transform: translate(0, -5px);
+    }
+
+`;
+const CardContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 20px;
+`;
+const Avatar = styled.div`
+  background-color: white;
+  border-radius: 50%;
+  border: 3px solid grey;
+  width: 140px;
+  height: 140px;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+`;
+const StyledImg = styled.img`
+  max-width: 60%;
+`;
+
+const CardText = styled.p`
+  font-size: 1.2em;
+`;
+export { CardText, StyledImg, Avatar, CardContent, Card, Container };

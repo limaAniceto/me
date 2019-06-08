45b42315de6109424967978e21c3b9e430949f8c
Miguel Aniceto <miguel.aniceto@mixcloud.com>
2019-06-08 17:07:14 +0100

    Responsive fixes on height and also temporarily removing the visualizations section

--- src/App.js
+++ src/App.js
@@ -1,37 +1,54 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import {Navbar, Nav, NavItem} from 'react-bootstrap';
-import Home from './Home/Home';
-import Technologies from './Technologies/Technologies';
-import Visualizations from './Visualizations/Visualizations';
-import styled from 'styled-components';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Navbar, Nav, NavItem } from "react-bootstrap";
+import Home from "./Home/Home";
+import Technologies from "./Technologies/Technologies";
+import Visualizations from "./Visualizations/Visualizations";
+import styled from "styled-components";
 
 class App extends Component {
-    render() {
-        return (
-            <Router>
-                <StyledContainer>
-                    <StyledNavBar fluid style={{marginBottom: "0"}} inverse staticTop>
-                        <Nav>
-                            <NavItem componentClass={Link} href={`${process.env.PUBLIC_URL}/`} to={`${process.env.PUBLIC_URL}/`} >Home</NavItem>
-                            <NavItem componentClass={Link} href={`${process.env.PUBLIC_URL}/vis`} to={`${process.env.PUBLIC_URL}/vis`} >Visualizations</NavItem>
-                        </Nav>
-                    </StyledNavBar>
-                    <Switch>
-                        <Route exact path={`${process.env.PUBLIC_URL}/`} component={Home}/>
-                        <Route exact path={`${process.env.PUBLIC_URL}/vis`} component={Visualizations}/>
-                    </Switch>
-                </StyledContainer>
-            </Router>
-        );
-    }
+  render() {
+    return (
+      <Router>
+        <StyledContainer>
+          <StyledNavBar fluid style={{ marginBottom: "0" }} inverse staticTop>
+            <Nav>
+              <NavItem
+                componentClass={Link}
+                href={`${process.env.PUBLIC_URL}/`}
+                to={`${process.env.PUBLIC_URL}/`}
+              >
+                Home
+              </NavItem>
+              {/* <NavItem
+                componentClass={Link}
+                href={`${process.env.PUBLIC_URL}/vis`}
+                to={`${process.env.PUBLIC_URL}/vis`}
+              >
+                Visualizations
+              </NavItem> */}
+            </Nav>
+          </StyledNavBar>
+          <Switch>
+            <Route exact path={`${process.env.PUBLIC_URL}/`} component={Home} />
+            <Route
+              exact
+              path={`${process.env.PUBLIC_URL}/vis`}
+              component={Visualizations}
+            />
+          </Switch>
+        </StyledContainer>
+      </Router>
+    );
+  }
 }
 const StyledNavBar = styled(Navbar)`
-    flex-direction: row;
+  flex-direction: row;
 `;
 
 const StyledContainer = styled.div`
-    display: flex;
-    flex-direction: column;
+  display: flex;
+  flex: 1;
+  flex-direction: column;
 `;
 export default App;

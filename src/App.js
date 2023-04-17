5049f0964c9fa5c702bfad328558fd18daf8c697
Miguel Aniceto <miguel.aniceto@hyperexponential.com>
2023-04-17 15:29:27 +0100

    [FE] Make the navbar mobile friendly

--- src/App.js
+++ src/App.js
@@ -3,40 +3,59 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { Navbar, Nav, NavItem } from "react-bootstrap";
 import Home from "./Home";
 import Visualizations from "./Visualizations";
-import ShowCase from './ShowCase';
+import ShowCase from "./ShowCase";
 import styled from "styled-components";
 
-
 const MenuLinks = [
-  { id: 'home', label: 'Home', href: `${process.env.PUBLIC_URL}/`, to: `${process.env.PUBLIC_URL}/`, component: Home },
-  { id: 'vis', label: 'Visualizations', href: `${process.env.PUBLIC_URL}/vis`, to: `${process.env.PUBLIC_URL}/vis`, component: Visualizations },
-  { id: 'showcase', shouldHide: true, label: 'Showcase', href: `${process.env.PUBLIC_URL}/showcase`, to: `${process.env.PUBLIC_URL}/showcase`, component: ShowCase }
+  {
+    id: "home",
+    label: "Home",
+    href: `${process.env.PUBLIC_URL}/`,
+    to: `${process.env.PUBLIC_URL}/`,
+    component: Home,
+  },
+  {
+    id: "vis",
+    label: "Visualizations",
+    href: `${process.env.PUBLIC_URL}/vis`,
+    to: `${process.env.PUBLIC_URL}/vis`,
+    component: Visualizations,
+  },
+  {
+    id: "showcase",
+    shouldHide: true,
+    label: "Showcase",
+    href: `${process.env.PUBLIC_URL}/showcase`,
+    to: `${process.env.PUBLIC_URL}/showcase`,
+    component: ShowCase,
+  },
 ];
 
 const App = () => (
   <Router>
     <StyledContainer>
-      <StyledNavBar fluid style={{ marginBottom: "0" }} inverse staticTop>
+      <Navbar fluid style={{ marginBottom: "0" }} inverse staticTop>
         <Nav>
-          {MenuLinks.filter(({shouldHide}) => !shouldHide).map(({ id, label, href, to }) => <NavItem
-            key={id}
-            componentClass={Link}
-            href={href}
-            to={to}
-          >
-            {label}
-          </NavItem>)}
+          {MenuLinks.filter(({ shouldHide }) => !shouldHide).map(
+            ({ id, label, href, to }) => (
+              <StyledNavItem key={id} componentClass={Link} href={href} to={to}>
+                {label}
+              </StyledNavItem>
+            )
+          )}
         </Nav>
-      </StyledNavBar>
+      </Navbar>
       <Switch>
-        {MenuLinks.map(({id, href, component}) => <Route key={id} exact path={href} component={component} />)}
+        {MenuLinks.map(({ id, href, component }) => (
+          <Route key={id} exact path={href} component={component} />
+        ))}
       </Switch>
     </StyledContainer>
   </Router>
 );
 
-const StyledNavBar = styled(Navbar)`
-  flex-direction: row;
+const StyledNavItem = styled(NavItem)`
+  float: left;
 `;
 
 const StyledContainer = styled.div`
